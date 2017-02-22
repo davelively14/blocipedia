b9ae94016035d1e954b77c229652a966307de393
Dave Lively <davelively14@gmail.com>
2017-02-21 19:22:56 -0500

    Properly displays or hides private wikis

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -2,4 +2,8 @@ module WikisHelper
   def is_authorized_to_make_private?
     current_user.admin? || current_user.premium?
   end
+
+  def is_authorized_to_view?(wiki)
+    !wiki.private || current_user == wiki.user || current_user.admin?
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module Blocipedia
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    # Loads secret keys an envornmental variables
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
