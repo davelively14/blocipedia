9de5169ce8e8a7ff02dd2f6a46d80dec65baf346
Dave Lively <davelively14@gmail.com>
2017-03-01 15:54:30 -0500

    Added bad credentials and redirect to deep page

--- spec/features/authentication.rb
+++ spec/features/authentication.rb
@@ -5,12 +5,33 @@ feature 'Authentication' do
   let(:password) { 'password' }
   let(:username) { 'new_user' }
 
-  scenario "signs in with correct credentials" do
+  before do
     create(:user, email: email, password: password, password_confirmation: password, username: username)
+  end
+
+  scenario "signs in with correct credentials" do
     visit(new_user_session_path)
     fill_in 'Email', with: email
     fill_in 'Password', with: password
     click_on 'Log in'
+    expect(page).to have_content('Signed in successfully.')
+  end
+
+  scenario "incorrect credientials denies sign-in and alerts user" do
+    visit(new_user_session_path)
+    fill_in 'Email', with: email
+    fill_in 'Password', with: 'nothinggood'
+    click_on 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  scenario "returns to deep page after sign-in" do
+    visit(wikis_path)
+    expect(current_path).to eq(new_user_session_path)
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_on 'Log in'
+    expect(current_path).to eq(wikis_path)
     expect(page).to have_content('Signed in successfully.')
   end
 end
